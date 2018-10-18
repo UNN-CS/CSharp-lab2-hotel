dad8591d8328601593199fe8a4b7cbc0d3e3f66a
Aleksandr Mizgirev <Test>
2018-10-18 16:52:08 +0300

    Lab2 Mizgirev

---
+++ src/InfoNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Lab2
+{
+    public class InfoNumbers
+    {
+        public Cnumber[] numbers { get; set; }
+
+    }
+
+    public class InfoServices
+    {
+        public Cnumber[] services { get; set; }
+    }
+
+    public class Cnumber
+    {
+        public string Type { get; set; }
+        public int Price { get; set; }
+    }
+}

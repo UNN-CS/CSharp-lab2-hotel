706d7113c424edeb231332f18cf0e0b5ff4c4050
Stasyan <stanislav.matveew@yandex.ru>
2018-12-16 23:44:23 +0300

    исправил баги, перезалил, уж так вышло

---
+++ scr/dopi_kortegi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel_maiami
+{
+    public class dopi_kortegi
+    {
+        public string name { get; set; }
+        public string price { get; set; }
+    }
+}
---
+++ scr/dopi_list.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hotel_maiami
+{
+    public class dopi_list
+    {
+        public dopi_kortegi[] Uslugi { get; set; }
+    }
+}

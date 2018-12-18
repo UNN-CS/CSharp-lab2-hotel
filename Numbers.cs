68627ec57810f7b4b9adbb89dade13dd93ce65ee
aYellowa <golo400@icloud.com>
2018-12-18 18:47:37 +0300

    1

---
+++ Numbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class NNumbers
+    {
+        public Num[] num { get; set; }
+    }
+    public class Servies
+    {
+        public Num[] servies { get; set; }
+    }
+    public class Num
+    {
+        public string Type { get; set; }
+        public int Price { get; set; }
+    }
+}

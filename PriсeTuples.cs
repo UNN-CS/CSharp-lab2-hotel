6c176c2ec3c2e943bdd2d7bb925d44fd12888263
Velnovikova <45849314+Velnovikova@users.noreply.github.com>
2018-12-27 04:45:50 +0300

    ...

---
+++ PriсeTuples.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Отель
+{
+   public class PriсeTuples
+    {
+        public string name { get; set; }
+        public string price { get; set; }
+    }
+}
